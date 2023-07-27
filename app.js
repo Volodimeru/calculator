3c7b9a6071609de6d8360f444d99a63423671863
volodimeru <vladi.dogonadze@gmail.com>
2023-07-27 21:23:57 +0400

    Added buttons, added variables for elements

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
+//variables for number buttons 
+const numbers = document.querySelectorAll(".button.buto");
+const wholeScreen = document.querySelector('.wholeScreen');
+const clearButton = document.querySelector('.butC');
+const allClearButton = document.querySelector('.butAC');
 
+console.log(allClearButton)
+
+numbers.forEach(number => number.addEventListener("click", function(e) {
+    console.log(e.target.innerText)
+}))
+allClearButton.addEventListener('click', function() {
+    wholeScreen.innerHTML = "";
+})
 function add (a,b) {
 return a+b
 };
