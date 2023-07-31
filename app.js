be04a028d50a93567dc35ad00b4fb2c7ed0b77fd
volodimeru <vladi.dogonadze@gmail.com>
2023-07-31 21:42:53 +0400

    I think Final

--- app.js
+++ app.js
@@ -3,100 +3,64 @@ const numbers = document.querySelectorAll(".button.buto");
 const wholeScreen = document.querySelector('.wholeScreen');
 const clearButton = document.querySelector('.butC');
 const allClearButton = document.querySelector('.butAC');
-const upScereen = document.querySelector(".butscreen.up");
 const recentScreen = document.querySelector(".butscreen.recent");
 const operations = document.querySelectorAll(".operation");
 const equalsButton = document.querySelector(".equals");
 
 
-
-
-
-
 let numbersText = [];
 let reCentNumbers = 0;
 let operator = 0;
-let answer = ''
+let answer = 0;
 
-
-
-
-// numbers.forEach(number => number.addEventListener("click", function(e) {
-//     numbersText.push(e.target.innerText);
-//     updateRecentScreen();
-// }));
-
-// function updateRecentScreen() {
-//     reCentNumbers = numbersText.join("");
-//     recentScreen.innerText = reCentNumbers;
-// }
 operations.forEach(operand => operand.addEventListener("click", operators));
 
 numbers.forEach(number => number.addEventListener("click", numbersOnScreen));
 
 clearButton.addEventListener('click', clearButtonAction)
-upScereen.innerText = ''
 recentScreen.innerText = ''
-allClearButton.addEventListener('click', function() {
-    operator = ''
-    reCentNumbers = '';
-    answer = '';
-    numbersText = [];
-    upScereen.innerText = ''
-    recentScreen.innerText = '0'
-})
 
+allClearButton.addEventListener('click', allClear);
+function allClear() {
+    operator = null
+    reCentNumbers = null;
+    answer = null;
+    numbersText = [];
+    recentScreen.innerText = null
+}
 function operators(e) {
         operator = e.target.innerText;
-        
-        firstNumber = reCentNumbers
-        upScereen.innerText = firstNumber+operator;
-        secondNumber = reCentNumbers
-        console.log("=",answer)
-
-         numbersText = [];
-    console.log("mi")
-    // firstNumber = reCentNumbers
-    console.log(firstNumber)
-    
-    recentScreen.innerText = ''
-    answer = operate(firstNumber,operator,secondNumber)
-    secondNumber=answer
+        firstNumber = recentScreen.innerText
+        numbersText = [];
 }
 
 function numbersOnScreen(e) {
     numbersText.push(e.target.innerText);
-    recentScreen.innerText = reCentNumbers
     for (i=0; i<numbersText.length; i++) {
         reCentNumbers = numbersText.join("")
         }
         recentScreen.innerText = reCentNumbers
-        console.log({reCentNumbers})
 }
 
 function clearButtonAction() {
     numbersText.pop();
     for (i=0; i<numbersText.length; i++) {
-        let reCentNumbers = numbersText.join("")
-        recentScreen.innerText=reCentNumbers
+        let reCentNu = numbersText.join("")
+        recentScreen.innerText=reCentNu
     }
     if (numbersText.length == 0){
         recentScreen.innerText = "0"
     }
 }
+
 function equality() {
     secondNumber=reCentNumbers;
-    upScereen.innerText = firstNumber+operator+secondNumber+"=";
-    console.log(firstNumber)
-    console.log(operator)
-    console.log(secondNumber)
     if (operator === "×") {
         operator = "*"
     }else if (operator === "÷") {
         operator = "/"
     }
-    let answer = operate(firstNumber,operator,secondNumber);
-    console.log("=",answer)
+    answer = operate(firstNumber,operator,secondNumber);
     recentScreen.innerText = answer;
     reCentNumbers=answer;
 }
@@ -105,22 +69,23 @@ equalsButton.addEventListener('click', equality);
 
 
 function add (a,b) {
-return a+b;
+return (a+b);
 };
 
 function subtract (a,b) {
-    return a-b;
+    return (a-b);
 };
 
 function multiply (a,b) {
-    return a*b;
+    return (a*b);
 };
 
 function divide (a,b) {
-    return a/b;
-};
+       return (a/b)
+     };
+
 function percent (a,b) {
-    return (a*b)/100;
+    return ((a*b)/100).toFixed(8);
 }
 
 function operate(firstNumber,operator,secondNumber){
@@ -134,8 +99,32 @@ function operate(firstNumber,operator,secondNumber){
         case '*' :
             return multiply(a,b);
         case '/' :
-            return divide(a,b);
+            if (b === 0) 
+                return "HEY! THIS IS NOT ALLOWED!"
+                else return divide(a,b)
         case '%' :
             return percent(a,b);
 }
 }
+
+window.addEventListener("keydown", function(e) {
+    console.log(e)
+    const num = ['1',"2","3","4","5","6","7","8","9","0","."];
+    const ope = ["+",'-',"*","/",'%'];
+    if (num.includes(e.key)) { 
+        console.log(e)
+        numbersText.push(e.key);
+        recentScreen.innerText = reCentNumbers;
+        for (i=0; i<numbersText.length; i++) {
+            reCentNumbers = numbersText.join("")
+            }
+            recentScreen.innerText = reCentNumbers;
+    } else if (ope.includes(e.key)) {
+        operator = e.key
+        firstNumber = recentScreen.innerText
+        numbersText = [];
+    } else if (e.key == 'Backspace') {clearButtonAction()
+    } else if (e.key == "=") {equality()
+    } else if (e.key == "Delete") {allClear()
+    }
+});
